b20e7686b8eee5aa7b9b52bb3e54207a208969d3
Douglas Hutchful <98133206+DouglasHutchful1@users.noreply.github.com>
2024-02-25 19:24:57 +0000

    navbar

---
+++ header.php
@@ -0,0 +1,61 @@
+<?php
+if(isset($message)){
+   foreach($message as $message){
+      echo '
+      <div class="message">
+         <span>'.$message.'</span>
+         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+      </div>
+      ';
+   }
+}
+?>
+
+<header class="header">
+
+    <div class="flex">
+
+    <a href="home.php" class="logo"><img src="./img/quirkylogo.JPG" width="130px"  alt="quirky"></a>
+
+        <nav class="navbar" style="margin-left:auto;">
+            <ul>
+                <li><a href="home.php">home</a></li>
+                <li><a href="#">Categories</a>
+                    <ul>
+                        <li><a href="about.php">Ghana Kaftan</a></li>
+                        <li><a href="contact.php">Shoes</a></li>
+                    </ul>
+                </li>
+                <li><a href="shop.php">new arrivals</a></li>
+                <li><a href="orders.php">orders</a></li>
+                <li><a href="shop.php">about us</a></li>
+                <li><a href="orders.php">contact</a></li>
+               
+            </ul>
+        </nav>
+
+        <div class="icons">
+            <div id="menu-btn" class="fas fa-bars"></div>
+            <a href="search_page.php" class="fas fa-search"></a>
+            <div id="user-btn" class="fas fa-user"></div>
+            <?php
+                $select_wishlist_count = mysqli_query($conn, "SELECT * FROM `wishlist` WHERE user_id = '$user_id'") or die('query failed');
+                $wishlist_num_rows = mysqli_num_rows($select_wishlist_count);
+            ?>
+            <a href="wishlist.php"><i class="fas fa-heart"></i><span>(<?php echo $wishlist_num_rows; ?>)</span></a>
+            <?php
+                $select_cart_count = mysqli_query($conn, "SELECT * FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
+                $cart_num_rows = mysqli_num_rows($select_cart_count);
+            ?>
+            <a href="cart.php"><i class="fas fa-shopping-cart"></i><span>(<?php echo $cart_num_rows; ?>)</span></a>
+        </div>
+
+        <div class="account-box">
+            <p>Fullname: <span><?php echo $_SESSION['user_name']; ?></span></p>
+            <p>Email Address: <span><?php echo $_SESSION['user_email']; ?></span></p>
+            <a href="logout.php" class="delete-btn">logout</a>
+        </div>
+
+    </div>
+
+</header>
